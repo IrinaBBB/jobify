d7d96c26344f59163281c10f32ca69d4b45dbbbc
IrinaB <balkarovairina@gmail.com>
2024-03-16 08:46:34 +0100

    Add Main Pages and Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+    HomeLayout,
+    Register,
+    Login,
+    Error,
+    Landing,
+    DashboardLayout, Admin, Profile, AllJobs, AddJob, Stats,
+} from './pages'
 
-function App(props) {
-    return (
-        <h1>Hello</h1>
-    )
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomeLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: 'register',
+                element: <Register />,
+            },
+            {
+                path: 'login',
+                element: <Login />,
+            },
+            {
+                path: 'dashboard',
+                element: <DashboardLayout />,
+                children: [
+                    {
+                        index: true,
+                        element: <AddJob />,
+                    },
+                    { path: 'stats', element: <Stats /> },
+                    {
+                        path: 'all-jobs',
+                        element: <AllJobs />,
+                    },
+
+                    {
+                        path: 'profile',
+                        element: <Profile />,
+                    },
+                    {
+                        path: 'admin',
+                        element: <Admin />,
+                    },
+                ],
+            },
+            {
+                path: 'landing',
+                element: <Landing />,
+            },
+        ],
+    },
+])
+
+const App = () => {
+    return <RouterProvider router={router} />
 }
-
 export default App
